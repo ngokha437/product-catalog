13e029b04f37bc9b4b0b32b161ffadbd7f71604d
Kha Ngo <ngokha437@gmail.com>
2026-02-10 10:47:47 +0700

    prepared for the deployment

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -15,7 +15,6 @@ const HomePage = () => {
     const fetchNotes = async () => {
       try {
         const res = await api.get("/notes");
-        console.log(res.data);
         setNotes(res.data);
         setIsRateLimited(false);
       } catch (error) {
